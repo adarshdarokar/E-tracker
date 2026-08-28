10fbcb4ce5f2b9424993a19b7212a5aa12a05f54
ADARSH DAROKAR <adarshdarokar55@gmail.com>
2026-08-28 23:55:57 +0530

    feat: reset expense form after submission

--- script.js
+++ script.js
@@ -1,64 +1,99 @@
 let balance = 0;
 
 let expenses = [];
+
 let editingIndex = null;
 
 const balanceElement = document.getElementById("balance");
+
 const form = document.getElementById("expenseForm");
+
 const titleInput = document.getElementById("title");
+
 const amountInput = document.getElementById("amount");
+
 const expenseList = document.getElementById("expenseList");
 
+
 function displayExpenses() {
-  expenseList.innerHTML = "";
 
-  expenses.forEach(function (expense, index) {
-    const li = document.createElement("li");
+    expenseList.innerHTML = "";
 
-    li.innerHTML = `${expense.title} - ₹${expense.amount}
-         <button data-index = "${index}">Delete</button>
+    expenses.forEach(function (expense, index) {
+
+        const li = document.createElement("li");
+
+        li.innerHTML = `${expense.title} - ₹${expense.amount}
+            
+            <button data-index="${index}">Delete</button>
+            
         `;
 
-    expenseList.appendChild(li);
-  });
+        expenseList.appendChild(li);
+
+    });
+
 }
+
 
 function calculateTotal() {
-  const total = expenses.reduce(function (sum, expense) {
-    return sum + expense.amount;
-  }, 0);
 
-  balance = -total;
-  balanceElement.textContent = `₹${balance}`;
+    const total = expenses.reduce(function (sum, expense) {
+
+        return sum + expense.amount;
+
+    }, 0);
+
+    balance = -total;
+
+    balanceElement.textContent = `₹${balance}`;
+
 }
 
+
 form.addEventListener("submit", function (event) {
-  event.preventDefault();
 
-  const title = titleInput.value;
-  const amount = Number(amountInput.value);
+    event.preventDefault();
 
-  const expense = {
-    title: title,
-    amount: amount,
-  };
+    const title = titleInput.value;
 
-  expenses.push(expense);
+    const amount = Number(amountInput.value);
 
-  displayExpenses();
-  calculateTotal();
+    const expense = {
+
+        title: title,
+
+        amount: amount,
+
+    };
+
+    expenses.push(expense);
+
+    displayExpenses();
+
+    calculateTotal();
+
+    form.reset();
+
 });
+
 
 expenseList.addEventListener("click", function (event) {
 
+    if (event.target.tagName === "BUTTON") {
 
-  
-  if (event.target.tagName === "BUTTON") {
-    const index = Number(event.target.dataset.index);
-    expenses = expenses.filter(function (expense, expenseIndex) {
-      return expenseIndex !== index;
-    });
-    displayExpenses();
-    calculateTotal();
-  }
+        const index = Number(event.target.dataset.index);
+
+        expenses = expenses.filter(function (expense, expenseIndex) {
+
+            return expenseIndex !== index;
+
+        });
+
+        displayExpenses();
+
+        calculateTotal();
+
+    }
+
 });
